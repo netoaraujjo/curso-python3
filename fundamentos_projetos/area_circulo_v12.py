03968aa0f738ea4461376f29fe445c39d20af2df
Neto Araujo <netoaraujjo@gmail.com>
2019-03-23 14:47:17 -0300

    Finalizado primeiro projeto

---
+++ fundamentos_projetos/area_circulo_v12.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from math import pi
+import sys
+
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+
+def help():
+    print("É necessário informar o raio do círculo.")
+    print(f"Sintaxe correta: {sys.argv[0]} <raio>")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+    else:
+        raio = sys.argv[1]
+        area = circulo(raio)
+        print('Área do círculo:', area)
---
+++ fundamentos_projetos/area_circulo_v13.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from math import pi
+import sys
+# import errno
+
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+
+def help():
+    print("É necessário informar o raio do círculo.")
+    print(f"Sintaxe correta: {sys.argv[0]} <raio>")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+        # sys.exit(errno.EPERM)
+    else:
+        raio = sys.argv[1]
+        area = circulo(raio)
+        print('Área do círculo:', area)
